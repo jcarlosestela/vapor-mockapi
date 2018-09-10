9e4956fa4bf93ba77cbac7e32a68425534d1d060
José Estela <jose.estela@gigigo.com>
2018-09-10 09:31:55 +0200

    New: Save responses in Gateway middleware

--- Sources/App/Middlewares/GatewayMiddleware.swift
+++ Sources/App/Middlewares/GatewayMiddleware.swift
@@ -15,6 +15,7 @@ final class GatewayMiddleware: Middleware {
     
     private let routes: [String]
     private let url: String
+    private let saveResponses: Bool
     
     // MARK: - Public methods
     
@@ -26,9 +27,11 @@ final class GatewayMiddleware: Middleware {
     /// - Parameters:
     ///   - url: The url of your final API
     ///   - routes: The routes to be ignored
-    init(to url: String, whenNotIn routes: [Route<Responder>]) {
+    ///   - savingResponses: Indicates to GateMiddleware if you want to save the responses of routes not included in the 'routes' param. (default false)
+    init(to url: String, whenNotIn routes: [Route<Responder>], savingResponses saveResponses: Bool = false) {
         self.routes = routes.readableRoutes()
         self.url = url
+        self.saveResponses = saveResponses
     }
     
     /// Instantiate a new GatewayMiddleware providing the url of your server API and the routes that you don't want to manage in the MockAPI
@@ -39,9 +42,11 @@ final class GatewayMiddleware: Middleware {
     /// - Parameters:
     ///   - url: The url of your final API
     ///   - routes: The routes to be ignored
-    init(to url: String, whenNotIn routes: [String]) {
+    ///   - savingResponses: Indicates to GateMiddleware if you want to save the responses of routes not included in the 'routes' param. (default false)
+    init(to url: String, whenNotIn routes: [String], savingResponses saveResponses: Bool = false) {
         self.routes = routes
         self.url = url
+        self.saveResponses = saveResponses
     }
     
     func respond(to request: Request, chainingTo next: Responder) throws -> Future<Response> {
@@ -56,9 +61,37 @@ final class GatewayMiddleware: Middleware {
             httpRequest.headers.remove(name: .host)
             let req = Request(http: httpRequest, using: request)
             let client = try request.client()
-            return client.send(req)
+            let response = client.send(req)
+            if self.saveResponses {
+                response.whenSuccess { response in
+                    self.saveResponse(response, of: request)
+                }
+            }
+            return response
         }
         return try next.respond(to: request)
+    }
+    
+    private func saveResponse(_ response: Response, of request: Request) {
+        guard
+            let responseData = response.http.body.data,
+            let jsonObject = try? JSONSerialization.jsonObject(with: responseData, options: .allowFragments),
+            let json = try? JSONSerialization.data(withJSONObject: jsonObject, options: .prettyPrinted)
+        else {
+            return
+        }
+        let body: String? = {
+            if let data = request.http.body.data {
+                return data.base64EncodedString()
+            }
+            return nil
+        }()
+        let path = request.http.url.path.convertToPathComponents().readable.replacingOccurrences(of: "/", with: "_") + ".json"
+        FileManager.default.createFile(
+            atPath: DirectoryConfig.detect().workDir + "Public/" + (body ?? "") + String(describing: request.http.method) + path,
+            contents: json,
+            attributes: nil
+        )
     }
 }
 
